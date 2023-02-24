6377c50935c230e7b5dbd2d997fc907dcf2598c4
PeteSargent <peter.winthrop.sargent@gmail.com>
2023-02-24 15:04:30 -0800

    Performed basic javascrip exercise

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+function My_First_Function() {                              //Defined and named the function.
+    var str = "This is the button text!";                   //Assigned the variable "str" a string value.
+    document.getElementById("Button_text").innerHTML = str; //Entered the Id for getElementById method to look for and the value to use when activated.
+}
+var Sent1 = "This is the beginning of the string";  //Assigned Sent1 first half of sentence.
+var Sent2 = " and this is the end of the string.";  //Assigned Sent2 secnd half of sentence. Space at begginning to prevent incorrect spacing.
+
+document.write(Sent1 + Sent2); //Expression that concatonates Sent1 and Sent2.
+window.alert("Here goes my first JS"); //Alert pop-up with message.
